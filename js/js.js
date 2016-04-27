dd11088a0e8eb88896fef2fc5c6575194f867ae8
Nate Finch <finchproservices@gmail.com>
2016-04-27 11:41:38 +0900

    update layout, update bower

--- js/js.js
+++ js/js.js
@@ -2,7 +2,12 @@
 
   $(document).ready(function () {
 
-    console.log('js and jquery are loading');
+    $('.js-accordion-trigger').bind('click', function(e){
+      jQuery(this).parent().find('.submenu').slideToggle('fast');  // apply the toggle to the ul
+      jQuery(this).parent().toggleClass('is-expanded');
+      e.preventDefault();
+    });
+
 
 
   });
